d7c681a26966fcf0208988e1372393c5cc960185
leeshajain <99830262+leeshajain@users.noreply.github.com>
2023-12-15 16:57:41 +0530

    Fix syntax error in getDataPoint function

--- client3.py
+++ client3.py
@@ -23,7 +23,7 @@ import random
 import urllib.request
 
 # Server API URLs
-QUERY = "http://localhost:8080/query?id={}"
+QUERY = "http://localhost:8000/query?id={}"
 
 # 500 server request
 N = 500
@@ -32,17 +32,27 @@ N = 500
 def getDataPoint(quote):
     """ Produce all the needed values to generate a datapoint """
     """ ------------- Update this function ------------- """
-    stock = quote['stock']
-    bid_price = float(quote['top_bid']['price'])
-    ask_price = float(quote['top_ask']['price'])
-    price = bid_price
+    stock = quote.get('stock')
+
+    if 'top_bid' in quote and 'top_ask' in quote:
+        bid_price = float(quote['top_bid']['price'])
+        ask_price = float(quote['top_ask']['price'])
+        price = bid_price
+
+    else:
+        bid_price, ask_price, price = 0.0, 0.0, 0.0
+
     return stock, bid_price, ask_price, price
 
 
 def getRatio(price_a, price_b):
     """ Get ratio of price_a and price_b """
     """ ------------- Update this function ------------- """
-    return 1
+    if price_b != 0:
+        return price_a / price_b
+    else:
+        return float('inf')
+
 
 
 # Main
@@ -52,8 +62,13 @@ if __name__ == "__main__":
         quotes = json.loads(urllib.request.urlopen(QUERY.format(random.random())).read())
 
         """ ----------- Update to get the ratio --------------- """
+        ratios = []
         for quote in quotes:
             stock, bid_price, ask_price, price = getDataPoint(quote)
             print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
+            ratios.append(getRatio(price, price))
 
-        print("Ratio %s" % getRatio(price, price))
+        average_ratio = sum(ratios) / len(ratios) if len(ratios) > 0 else 0
+        print("Average Ratio: %s" % average_ratio)
+
+
